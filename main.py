58457906c9a9ff878bd08dfc66c945b3acf5a9f0
Daniyal Khaliq <daniyal.khaliq@empglabs.com>
2022-07-07 15:18:11 +0500

    gitginore and test case added

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def func2(a, b, c) -> str:
     return os.getcwd()
 
 
+def subtract(a, b) -> int:
+    return a - b
+
+
+def pow(a, b) -> int:
+    return pow(a, b)
+
+
 def to_sentence(s) -> str:
     s = s.capitalize()
     if s.endswith("."):
--- test/test_main.py
+++ test/test_main.py
@@ -4,3 +4,8 @@ import main
 def test_add():
     assert main.add(3, 4) == 7
     assert main.add(3.5, 4) == 7
+
+
+def test_subract():
+    assert main.subtract(8, 3) == 5
+    assert main.subtract(4, 2) == 2
